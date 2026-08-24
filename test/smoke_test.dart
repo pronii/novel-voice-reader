5b56715b25e86130c3c0f9eab883913c268ca66a
Administrator <pronii@88.com>
2026-08-24 11:10:13 +0800

    test(library): fix UI assertions for wooden bookshelf redesign
    - smoke_test: expect '我的书架' header instead of the old '书架' AppBar title
    - navigation_test: expect the '添加书籍' shelf placeholder instead of the
      removed floating import button tooltip

--- test/smoke_test.dart
+++ test/smoke_test.dart
@@ -5,7 +5,7 @@ void main() {
   testWidgets('opens the library as the first screen', (tester) async {
     await tester.pumpWidget(const NovelVoiceReaderApp());
 
-    expect(find.text('书架'), findsOneWidget);
+    expect(find.text('我的书架'), findsOneWidget);
     expect(find.text('还没有导入小说'), findsOneWidget);
   });
 }
